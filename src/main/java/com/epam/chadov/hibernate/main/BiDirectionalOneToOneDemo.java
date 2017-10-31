57898657908462e157b0b41691b6d0a0f785bc64
Andrey_Chadov <Andrey_Chadov@epam.com>
2017-10-31 14:15:44 +0600

    [IMP] Implemented BiDirectionalOneToOneDemo

---
+++ src/main/java/com/epam/chadov/hibernate/main/BiDirectionalOneToOneDemo.java
@@ -0,0 +1,46 @@
+package com.epam.chadov.hibernate.main;
+
+import com.epam.chadov.hibernate.entities.Instructor;
+import com.epam.chadov.hibernate.entities.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * @author Andrey_Chadov on 10/31/2017.
+ */
+public class BiDirectionalOneToOneDemo {
+
+    public static void main(String[] args) {
+
+        SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+
+        try (Session session = sessionFactory.getCurrentSession()) {
+            //start a transaction
+            session.beginTransaction();
+
+            //get the instructor detail object
+            int theId = 2;
+            InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class, theId);
+
+            //print detail
+            System.out.println("tempInstructorDetail : " + tempInstructorDetail);
+
+
+            //print the associated instructor
+            System.out.println("the associated instructor : " + tempInstructorDetail.getInstructor());
+
+            //commit transaction
+            session.getTransaction().commit();
+            System.out.println("Done!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            sessionFactory.close();
+        }
+    }
+}
