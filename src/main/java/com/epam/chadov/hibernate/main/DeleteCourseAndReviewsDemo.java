a33f638e41ad819f9ea3762377eefba9b153343c
Andrey_Chadov <Andrey_Chadov@epam.com>
2017-11-15 14:47:20 +0600

    [IMP]           Implemented one to many uni directional - delete action with cascade.all

--- src/main/java/com/epam/chadov/hibernate/main/DeleteCourseAndReviewsDemo.java
+++ src/main/java/com/epam/chadov/hibernate/main/DeleteCourseAndReviewsDemo.java
@@ -3,6 +3,7 @@ package com.epam.chadov.hibernate.main;
 import com.epam.chadov.hibernate.entities.Course;
 import com.epam.chadov.hibernate.entities.Instructor;
 import com.epam.chadov.hibernate.entities.InstructorDetail;
+import com.epam.chadov.hibernate.entities.Review;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -10,7 +11,7 @@ import org.hibernate.cfg.Configuration;
 /**
  * @author Andrey_Chadov on 10/30/2017.
  */
-public class DeleteCourseDemo {
+public class DeleteCourseAndReviewsDemo {
 
     public static void main(String[] args) {
 
@@ -18,20 +19,19 @@ public class DeleteCourseDemo {
                 .addAnnotatedClass(Instructor.class)
                 .addAnnotatedClass(InstructorDetail.class)
                 .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Review.class)
                 .buildSessionFactory();
-
 
         try (Session session = sessionFactory.getCurrentSession()) {
             //start a transaction
             session.beginTransaction();
 
-            //get a course
-            int theId = 10;
-
+            //get the course
+            int theId = 12;
             Course tempCourse = session.get(Course.class, theId);
 
-            //delete a course and not the associated instructor (only the course)
-            System.out.println(" Deleting course : " + tempCourse);
+            System.out.println("Deleting the course ..." + tempCourse);
+            System.out.println("reviews : " + tempCourse.getReviews());
             session.delete(tempCourse);
 
             //commit transaction
